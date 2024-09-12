fd778bbf78eff4c921132bdb5a071fce49851f9a
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-09-12 18:15:11 +0500

    In this project i made birthday wish with the help of react component,framer-motion , use card component and react icons

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import BirthdayWish from "@/components/BirthdayWish";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <BirthdayWish />
+    </div>
+  );
+}
